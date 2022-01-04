adc175b09e3cf0db7c466ee9e7201f89d39df70c
Kyle McCabe <kmccab206@caledonian.ac.uk>
2022-01-04 21:13:43 +0000

    Added Logging

--- tech-test/src/main/java/com/kylmcb/techtest/controller/SmartReadsController.java
+++ tech-test/src/main/java/com/kylmcb/techtest/controller/SmartReadsController.java
@@ -4,6 +4,7 @@ import com.kylmcb.techtest.database.AccountRepository;
 import com.kylmcb.techtest.database.ElecReadingRepository;
 import com.kylmcb.techtest.database.GasReadingRepository;
 import com.kylmcb.techtest.models.Account;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/api")
 public class SmartReadsController {
@@ -26,10 +28,13 @@ public class SmartReadsController {
 
     @GetMapping(path = "/smart/reads/{account_id}")
     public ResponseEntity<Account> getAccountById(@PathVariable("account_id") long accountId) {
+        log.info("Finding account with accountId {}", accountId);
         Account account = accountRepository.findById(accountId);
         if (account != null) {
+            log.info("Account found with accountId {}: {}", accountId, account);
             return new ResponseEntity<>(account, HttpStatus.OK);
         } else {
+            log.info("No account found with accountId {}", accountId);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
