c102d30d66d412958f6f8a0d9a34a9f8117844ff
Kyle McCabe <kmccab206@caledonian.ac.uk>
2022-01-04 20:10:00 +0000

    Created Elec & Gas Reading Models

--- tech-test/src/main/java/com/kylmcb/techtest/models/ElecReading.java
+++ tech-test/src/main/java/com/kylmcb/techtest/models/ElecReading.java
@@ -1,4 +1,27 @@
 package com.kylmcb.techtest.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "elec_reading")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ElecReading {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private int meterId;
+    private int reading;
+    private LocalDate date;
+
+    @ToString.Exclude
+    @ManyToOne()
+    @JoinColumn(name = "account_id")
+    private Account account;
 }
