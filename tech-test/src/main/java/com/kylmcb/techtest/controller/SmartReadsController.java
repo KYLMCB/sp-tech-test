0bf796abd2bee102deb842d83febfd82f6ced659
Kyle McCabe <kmccab206@caledonian.ac.uk>
2022-01-04 20:48:25 +0000

    Autowiring Repository classes in Controller

--- tech-test/src/main/java/com/kylmcb/techtest/controller/SmartReadsController.java
+++ tech-test/src/main/java/com/kylmcb/techtest/controller/SmartReadsController.java
@@ -1,4 +1,21 @@
 package com.kylmcb.techtest.controller;
 
+import com.kylmcb.techtest.database.AccountRepository;
+import com.kylmcb.techtest.database.ElecReadingRepository;
+import com.kylmcb.techtest.database.GasReadingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
 public class SmartReadsController {
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    ElecReadingRepository elecReadingRepository;
+
+    @Autowired
+    GasReadingRepository gasReadingRepository;
 }
