fca945ab0ceb3e1ccc98aedadfe7db3c573f65d7
Kyle McCabe <kmccab206@caledonian.ac.uk>
2022-01-04 20:53:36 +0000

    Added GET endpoint and printing account after findById

--- tech-test/src/main/java/com/kylmcb/techtest/controller/SmartReadsController.java
+++ tech-test/src/main/java/com/kylmcb/techtest/controller/SmartReadsController.java
@@ -3,7 +3,12 @@ package com.kylmcb.techtest.controller;
 import com.kylmcb.techtest.database.AccountRepository;
 import com.kylmcb.techtest.database.ElecReadingRepository;
 import com.kylmcb.techtest.database.GasReadingRepository;
+import com.kylmcb.techtest.models.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +23,10 @@ public class SmartReadsController {
 
     @Autowired
     GasReadingRepository gasReadingRepository;
+
+    @GetMapping(path = "/smart/reads/{account_id}")
+    public void getAccountById(@PathVariable("account_id") long account_id) {
+        Account account = accountRepository.findById(account_id);
+        System.out.println(account);
+    }
 }
