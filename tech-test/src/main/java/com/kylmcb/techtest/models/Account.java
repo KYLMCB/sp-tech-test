de3b8ec97deb1d6faedd82d03582befd7fa37237
Kyle McCabe <kmccab206@caledonian.ac.uk>
2022-01-04 20:06:26 +0000

    Created Account Model

--- tech-test/src/main/java/com/kylmcb/techtest/models/Account.java
+++ tech-test/src/main/java/com/kylmcb/techtest/models/Account.java
@@ -1,4 +1,29 @@
 package com.kylmcb.techtest.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long accountId;
+
+    @OneToMany(mappedBy = "account",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<GasReading> gasReadings;
+
+    @OneToMany(mappedBy = "account",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<ElecReading> elecReadings;
+
 }
